032d67abf8af6d085e901984c72499cfbfd6e89c
GuillermoFloresV <guillermo.fifth@gmail.com>
2021-10-28 13:00:19 -0700

    Initial Commit

---
+++ src/main/java/edu/csumb/Project3_Group12_backend/Project3Group12BackendApplication.java
@@ -0,0 +1,13 @@
+package edu.csumb.Project3_Group12_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3Group12BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3Group12BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/csumb/Project3_Group12_backend/Project3Group12BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.csumb.Project3_Group12_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3Group12BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
